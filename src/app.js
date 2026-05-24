2da43a9456ba5ade7868846b0a7a5b35539b9e44
husan <husanarman19@gmail.com>
2026-05-24 13:48:14 +0530

    created a repo

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+ const app = express();
+
+ app.listen(3000, () => {
+     console.log('Server is running on port 3000');
+ });
+
+ app.get('/', (req, res) => {
+     res.send('Welcome to Dev Tinder!');
+ });
+ app.get('/test', (req, res) => {
+     res.send('This is a test route for Dev Tinder!');
+ });
+ app.get('/hello', (req, res) => {
+     res.send('Hello, Dev Tinder!');
+ });
+app.get('/users', (req, res) => {
+    res.send('This is the users route for Dev Tinder!');
+});
+
