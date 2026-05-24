954994c95cad802155af6d04cd031d6fa98e0d17
husan <husanarman19@gmail.com>
2026-05-24 14:24:12 +0530

    routing done

--- src/app.js
+++ src/app.js
@@ -5,16 +5,26 @@ const express = require('express');
      console.log('Server is running on port 3000');
  });
 
- app.get('/', (req, res) => {
-     res.send('Welcome to Dev Tinder!');
- });
- app.get('/test', (req, res) => {
-     res.send('This is a test route for Dev Tinder!');
- });
- app.get('/hello', (req, res) => {
-     res.send('Hello, Dev Tinder!');
- });
+//  app.get('/', (req, res) => {
+//      res.send('Welcome to Dev Tinder!');
+//  });
+//  app.get('/test', (req, res) => {
+//      res.send('This is a test route for Dev Tinder!');
+//  });
+//  app.get('/hello', (req, res) => {
+//      res.send('Hello, Dev Tinder!');
+//  });
 app.get('/users', (req, res) => {
     res.send('This is the users route for Dev Tinder!');
 });
+
+app.post('/users', (req, res) => {
+    res.send('This is the login route for Dev Tinder!');
+});
+app.delete('/users', (req, res) => {
+    res.send('This is the logout route for Dev Tinder!');
+});
+app.use('/users', (req, res) => {
+    res.send('This is the users route for Dev Tinder!');
+}   );
 
